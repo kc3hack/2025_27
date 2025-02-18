5b4ef56fbb9994fcf43c145c0d8c0dbe986dbf1a
Hideyasu <kutuzov1228@gmail.com>
2025-02-19 01:08:42 +0900

    feat: 微修正

--- frontend/src/routes/design/top/+layout.svelte
+++ frontend/src/routes/design/top/+layout.svelte
@@ -1,6 +1,7 @@
 <script>
 	import { i18n } from '$lib/i18n';
 	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
+	import './layout.css';
 	import '$src/app.css';
 	// ページコンテンツを受け取る
 	let { children } = $props();
