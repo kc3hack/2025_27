9c35ec0b9645d24d7bf503b619607ca2ef278dbb
Kkk0504 <kikka.takami@icloud.com>
2025-02-18 14:25:38 +0900

    main追加

--- frontend/src/routes/design/top/+page.svelte
+++ frontend/src/routes/design/top/+page.svelte
@@ -7,15 +7,42 @@
 </header>
 
 <main>
-    <section class="user">
-        <p></p>
+    <!-- アバター、ニックネーム設定 -->
+    <section class="user-setup">
+        <p>アバターとニックネームを設定してください</p>
     </section>
+
+    <!-- 遊び方書く -->
+    <section class="how-to-use">
+        <h2>遊び方</h2>
+        <p>説明書く</p>
+    </section>  
 </main>
+
+<footer class="footer">
+    <h1>WARA1</h1>
+</footer>
 
 <style>
     .header {
         color: #000000;
         padding: 5rem;
+        text-align: center;
+    }
+
+    .main {
+        display: flex;
+        justify-content: space-between; 
+        background-color: #a3892e;
+    }
+
+    .user-setup {
+        width: 70%;
+        text-align: center;
+    }
+
+    .how-to-use {
+        width: 30%;
         text-align: center;
     }
 </style>
