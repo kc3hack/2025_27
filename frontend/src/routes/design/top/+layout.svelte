526ebc92b4a56f24892d0d1ad490b068319572e3
Hideyasu <kutuzov1228@gmail.com>
2025-02-19 00:59:18 +0900

    feat: css を tailwindで指定するように変更

--- frontend/src/routes/design/top/+layout.svelte
+++ frontend/src/routes/design/top/+layout.svelte
@@ -7,5 +7,14 @@
 </script>
 
 <ParaglideJS {i18n}>
+	<header class="header w-full">
+		<div class="flex justify-center"><h1 class="text-4xl">WARA1</h1></div>
+	</header>
 	<div class="main">{@render children()}</div>
+	<footer class="footer w-full">
+		<div class="flex"><p class="text-2xl">WARA1</p></div>
+	</footer>
 </ParaglideJS>
+
+<style>
+</style>
--- frontend/src/routes/design/top/+page.svelte
+++ frontend/src/routes/design/top/+page.svelte
@@ -1,87 +1,31 @@
 <script>
-    let name ="";
+	let name = '';
 </script>
 
-<div class="container">
-    <header class="header">
-        <h1>WARA1</h1>
-    </header>
-
-    <main class="main">
-        <!-- アバター、ニックネーム設定 -->
-        <section class="user-setup">
-            <p class="setup-text">アバターとニックネームを設定してください</p>
-            <input type="text" bind:value={name} placeholder="名前を入力"/>
-            <button class="start-button">スタート</button>
-        </section>
-
-        <!-- 遊び方書く -->
-        <section class="how-to-use">
-            <h2>遊び方</h2>
-            <p>ここに説明書く</p>
-        </section>  
-    </main>
-
-    <footer class="footer">
-        <p>WARA1</p>
-    </footer>
-</div>
+<main class="content h-250 container mx-auto bg-amber-300">
+	<!-- アバター、ニックネーム設定 -->
+	<section class="col-6 user-setup bg-amber-500">
+		<p class="setup-text">アバターとニックネームを設定してください</p>
+		<input type="text" bind:value={name} placeholder="名前を入力" />
+		<button class="start-button">スタート</button>
+	</section>
+	<section class="col-6 how-to-use bg-amber-800">
+		<!-- 遊び方書く -->
+		<h2>遊び方</h2>
+		<p>ここに説明書く</p>
+	</section>
+</main>
 
 <style>
-    html, body {
-        width: 100vw;
-        height: 100vh;
-        margin: 0;
-        padding: 0;
-        overflow: hidden;
-        display: flex;
-        flex-direction:column;
-        align-items: center;
-    }
-    .container {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        height: 100vh;
-        width: 100vw;
-    }
-    .header {
-        width: 100%;
-        text-align: center;
-        font-size: 4vw;
-        font-weight: bold;
-        padding: 1vh 0;
-    }
+	.content {
+	}
 
-    .main {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        flex-grow: 1;
-        width: 150%;
-        height: 60vh;
-        background-color: #a3892e;
-        padding: 2vw;
-        border-radius: 10px;
-    }
+	.user-setup {
+	}
 
-    .user-setup {
-        width: 70%;
-        text-align: center;
-    }
+	.setup-text {
+	}
 
-    .setup-text {
-
-    }
-
-    .how-to-use {
-        width: 30%;
-        text-align: center;
-    }
-
-    .footer {
-        margin-top: 20px;
-        font-weight: bold;
-    }
+	.how-to-use {
+	}
 </style>
