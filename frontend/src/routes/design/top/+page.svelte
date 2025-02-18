9b76c3e249cdd638064382202abf30c128475e71
Kkk0504 <kikka.takami@icloud.com>
2025-02-18 14:01:49 +0900

    タイトル

---
+++ frontend/src/routes/design/top/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+
+</script>
+
+<header class="header">
+    <h1>WARA1</h1>
+</header>
+
+<main>
+    <section class="user">
+        <p></p>
+    </section>
+</main>
+
+<style>
+    .header {
+        color: #000000;
+        padding: 5rem;
+        text-align: center;
+    }
+</style>
