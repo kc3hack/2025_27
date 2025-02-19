49b516fc50cc3780f6e6116707a4a1225cb06bbe
Kkk0504 <kikka.takami@icloud.com>
2025-02-19 21:30:03 +0900

    7:3に分けた

--- frontend/src/routes/design/top/+layout.svelte
+++ frontend/src/routes/design/top/+layout.svelte
@@ -9,7 +9,7 @@
 
 <ParaglideJS {i18n}>
 	<header class="header w-full">
-		<div class="flex justify-center"><h1 class="text-4xl">WARA1</h1></div>
+		<div class="flex justify-center"><h1 class="text-5xl">WARA1</h1></div>
 	</header>
 	<div class="main">{@render children()}</div>
 	<footer class="footer w-full">
--- frontend/src/routes/design/top/+page.svelte
+++ frontend/src/routes/design/top/+page.svelte
@@ -2,14 +2,14 @@
 	let name = '';
 </script>
 
-<main class="content h-250 container mx-auto bg-amber-300">
+<main class="content h-250 container mx-auto bg-amber-300 flex justify-betwee">
 	<!-- アバター、ニックネーム設定 -->
-	<section class="col-6 user-setup bg-amber-500">
+	<section class="col-6 user-setup w-7/10 bg-amber-500">
 		<p class="setup-text">アバターとニックネームを設定してください</p>
 		<input type="text" bind:value={name} placeholder="名前を入力" />
 		<button class="start-button">スタート</button>
 	</section>
-	<section class="col-6 how-to-use bg-amber-800">
+	<section class="col-6 how-to-use w-3/10 bg-amber-800">
 		<!-- 遊び方書く -->
 		<h2>遊び方</h2>
 		<p>ここに説明書く</p>
