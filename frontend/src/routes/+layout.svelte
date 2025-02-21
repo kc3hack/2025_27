07595fd65d65c59f98b32d8ecaaf46296cc37254
Hideyasu <kutuzov1228@gmail.com>
2025-02-21 23:19:27 +0900

    feat: layout.svelteの適用をページごとに切り替えれるようにした

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script>
 	import { i18n } from '$lib/i18n';
+	import { page } from '$app/stores';
 	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
 	import '$src/app.css';
 	import Header from '$src/components/Header.svelte';
@@ -10,9 +11,13 @@
 </script>
 
 <ParaglideJS {i18n}>
-	<Header />
-	<div class="main">{@render children()}</div>
-	<Footer />
+	{#if $page.url.pathname.includes('game_answer') || $page.url.pathname.includes('game_master') || $page.url.pathname.includes('result')}
+		<div class="main">{@render children()}</div>
+	{:else}
+		<Header />
+		<div class="main">{@render children()}</div>
+		<Footer />
+	{/if}
 </ParaglideJS>
 
 <style>
