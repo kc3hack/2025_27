494bc28d73871c8f8d9770d4be35b993626fbe37
Kkk0504 <kikka.takami@icloud.com>
2025-02-21 23:55:57 +0900

    お題のsection

---
+++ frontend/src/routes/design/top/[lobby]/game_answer/+page.svelte
@@ -0,0 +1,11 @@
+<script>   
+</script>
+<main class="content h-250 container mx-auto flex items-center justify-between bg-amber-300">
+    <!-- お題表示の部分 -->
+    <section class= "w-full bg-yellow-500 p-7 text-6xl rounded-md text-center text-black">
+        お題:ボウリングで全ての投球がガターだった時、<br>
+        起こることは？
+    </section>
+</main>
+
+
