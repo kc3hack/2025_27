d53a49b24ee012ca0d426f1154a7b4f893909ce6
Kkk0504 <kikka.takami@icloud.com>
2025-02-22 00:14:23 +0900

    お絵描きののsection

--- frontend/src/routes/design/top/[lobby]/game_answer/+page.svelte
+++ frontend/src/routes/design/top/[lobby]/game_answer/+page.svelte
@@ -1,10 +1,38 @@
-<script>   
+<script>  
+    import Canvas from './Canvas.svelte'
+	import Palette from './Palette.svelte'
+
+    const colors = [
+		'#d58141',
+		'#d7c44c',
+		'#4fa9cc',
+		'#3f8d27',
+	]
+	const background = '#fff'
+
+	let color = colors[0]
+	const paletteColor = color
 </script>
-<main class="content h-250 container mx-auto flex items-center justify-between bg-amber-300">
+<main class="content h-250 container mx-auto flex flex-col items-center justify-between bg-amber-300">
     <!-- お題表示の部分 -->
-    <section class= "w-full bg-yellow-500 p-7 text-6xl rounded-md text-center text-black">
-        お題:ボウリングで全ての投球がガターだった時、<br>
-        起こることは？
+    <section class= "w-full bg-yellow-500 p-10 text-5xl rounded-md text-center text-black">
+        お題:ボウリングで全ての投球がガターだった時、起こることは？
+    </section>
+
+    <!-- お絵描き部分 -->
+    <section class="p-6 rounded-md flex mt-6 items-center">
+        <div class="Drawing section">
+            <h3>お絵描き</h3>
+            <Canvas {color} {background} />
+            <Palette 
+                {paletteColor}
+                {colors}
+                {background}
+                on:color="{({ detail }) => {
+                    color = detail.color;
+                }}"
+            />
+        </div>
     </section>
 </main>
 
