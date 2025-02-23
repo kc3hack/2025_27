2912fae5911a8e46aba4fde22c8b7e0c4bbf0519
Neon-straySheep <skyskysky1228t@gmail.com>
2025-02-23 17:30:47 +0900

    テールウィンドへの変換大体終わりました！

---
+++ frontend/src/components/UserNameSlot.svelte
@@ -0,0 +1,13 @@
+<script>
+    export let icon ='';
+    export let name ='';
+</script>
+
+<dev class="bg-yellow-200 grid grid-cols-7 gap-0 border-solid border-2 m-[5px]">
+    <div class="flex justify-center items-center">
+        <img src={icon} alt="icon画像" width="70px">
+    </div>
+    <dev class="sm:text-3xl md:text-4xl lg:text-5xl col-start-2 col-end-8">
+        {name}
+    </dev>
+</dev>
--- frontend/src/routes/design/top/[lobby]/+page.svelte
+++ frontend/src/routes/design/top/[lobby]/+page.svelte
@@ -1,168 +1,30 @@
-
 <script>
-	
-    import { goto } from '$app/navigation';
-
-    function navigate() {
-        goto('/ogiri'); // /ogiri へ移動
-    }
+	import UserInviteDecide from '$src/components/UserInvite&Decide.svelte';
+	import UserNameSlot from '$src/components/UserNameSlot.svelte';
+    
+    let name ='バカリズム';
+    let icon ='https://i.pinimg.com/736x/bf/fa/67/bffa675de717844c2b0a9f2bc42a5c04.jpg';
+    /* 多分関数とかで人数分取ってこれば良き */
 </script>
 
-<div class="lobby">
-    <div class="title">
-        <div class="intitle">
-            WARA-1
-        </div>
-    </div> 
-
-    <div class="memberSide">      
-        <dev class="memberList">
-            <dev class="member">
-                <dev class="icon">
-                    icon(予)
-                </dev>
-                <dev class="memName">
-                    粗品A
-                </dev>
-            </dev>
-            <dev class="member">
-                <dev class="icon">
-                    icon(予)
-                </dev>
-                <dev class="memName">
-                    粗品B
-                </dev>
-            </dev>
-            <dev class="member">
-                <dev class="icon">
-                    icon(予)
-                </dev>
-                <dev class="memName">
-                    粗品C
-                </dev>
-            </dev>
-            <dev class="member">
-                <dev class="icon">
-                    icon(予)
-                </dev>
-                <dev class="memName">
-                    粗品D
-                </dev>
-            </dev>
-            <dev class="member">
-                <dev class="icon">
-                    icon(予)
-                </dev>
-                <dev class="memName">
-                    バカリズム
-                </dev>
-            </dev>
+<div class="grid grid-cols-2 bg-amber-200 ">
+    <div class=" bg-amber-300 flex justify-center items-center m-[5px]">      
+        <dev class="col-start-1 col-end-2 flex justify-center items-center flex-col m-[5px]">
+            <div class="flex justify-center items-center bg-lime-300 w-[200px] border border-black border-solid rounded-[40%]">
+                joined member
+            </div>
+            <!-- 各メンバーに変数を当てて表示する メンバー追加ごとにコンポーネントを生やせると良き-->
+            <UserNameSlot {icon} {name} />
+            <UserNameSlot {icon} {name} />
+            <UserNameSlot {icon} {name} />
+            <UserNameSlot {icon} {name} />
+            <UserNameSlot {icon} {name} />
         </dev>
     </div>
 
-    <dev class="start">
-        <img src="https://www.fujitv.co.jp/ippon/img/logo_ogp.png" alt="IPPONグランプリ画像" width="90%">
-        <dev class="touch">
-            <dev class="item">
-                招待
-            </dev>
-
-            <dev class="item">
-                <button on:click={navigate}>スタート</button>
-            </dev>
-        </dev>
+    <dev class="col-start-2 col-end-3 ">
+        <UserInviteDecide />
     </dev>
 </div>
 
-<style>
-    .lobby{
-        display: grid;
-        grid-template-areas: 
-        "title title"
-        "memberList start";
-        background-color: rgba(174, 174, 20, 0.71);
-        grid-template-columns: 5fr 5fr;
-        grid-template-rows: 1fr 4fr;
-        height: 100vh;
-    }
-
-    .lobby .title{
-        grid-area: title;
-        display: flex;
-        justify-content: center; /* 横軸中央 */
-        align-items: center; /* 縦軸中央 */
-        background-color: rgba(132, 132, 15, 0.71);
-        font-size: 10vh;
-    }
-
-    .lobby .title .intitle{
-        display: grid;
-        place-items: center; /* 二つ合わせて縦横中央 */
-        background-color: rgba(174, 174, 20, 0.71);
-        padding: 0px 20px;
-    }
-
-    .lobby .memberSide{
-        background-color: rgba(176, 127, 23, 0.71);
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        margin: 5%;
-    }
-
-    .lobby .memberSide .memberList{
-        grid-area: memberList;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        flex-direction: column;
-    }
-
-    .lobby .memberSide .memberList .member{
-        grid-template-areas:
-        "icon memName";
-        grid-template-columns: 5.9vh auto;
-        background-color: rgba(132, 132, 15, 0.71);
-        width: 100%;
-        margin: 0.9vh;
-    }
-
-    .lobby .memberSide .memberList .member .icon{
-        grid-area: icon;
-    }
-
-    .lobby .memberSide .memberList .member .memName{
-        grid-area: memName;
-        font-size: 5vh;
-    }
-
-    .lobby .start{
-        grid-area: start;
-        display: grid;
-        grid-template-rows: 6fr 1fr 1fr 2fr;
-        
-    }
-
-    .lobby .start .touch{
-        grid-row-start: 3;
-        grid-row-end: 4;
-        display: flex;
-    }
-
-    .lobby .start .touch .item{
-
-        background-color: rgb(206, 246, 161);
-        width: 40%;
-        margin-left: 5%;
-        margin-right: 5%;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        font-size: 3vh;
-    }
-
-    img{
-        margin: 5%;
-        
-    }
-</style>
+<style></style>
