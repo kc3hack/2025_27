95579e727a322436d507c3c468c6575dfee8c52e
Neon-straySheep <skyskysky1228t@gmail.com>
2025-02-24 00:11:18 +0900

    lobby画面のフロントバック合体できました！(多分)

--- frontend/src/routes/[lobby]/+page.svelte
+++ frontend/src/routes/[lobby]/+page.svelte
@@ -4,6 +4,7 @@
 	import { page } from '$app/stores';
 	import { connectWebSocket } from '$lib/chat.js';
 	import { goto } from '$app/navigation';
+	import UserNameSlot from '$src/components/UserNameSlot.svelte';
 	let socket;
 	let lobbyId = '';
 	let nickname = '';
@@ -72,13 +73,32 @@
 	});
 </script>
 
-<h1>Lobby</h1>
-<p>lobbyId: {lobbyId}</p>
-{#each players as _, index}
-	<p>player{index + 1}: {_.nickname} {_.image}</p>
-{/each}
-<p>nickname: {nickname}</p>
-<p>selectAvata: {selectAvatar}</p>
+<div class="grid grid-cols-2 bg-amber-200 ">
+    <div class=" bg-amber-300 flex justify-center items-center m-[5px]">      
+        <div class="col-start-1 col-end-2 flex justify-center items-center flex-col m-[5px]">
+            <div class="text-amber-400 flex justify-center items-center bg-gray-800 w-[200px] border border-black border-solid rounded-[40%]">
+                joined members
+            </div>
+            <!-- 各メンバーに変数を当てて表示する メンバー追加ごとにコンポーネントを生やせると良き-->
+			<!-- 今日の教訓！！！！親コンポーネントで仮置きした変数と実際に代入する変数は同じ名前にしておかないと死ぬぞ！！ -->
+			{#each players as _, index}
+			<UserNameSlot icon={_.image} name={_.nickname} />
+			{/each}
+		</div>
+    </div>
 
-<button on:click={startGame}>start_game</button>
-<button on:click={inviteLink}>invite link</button>
+    <div class="col-start-2 col-end-3 ">
+		<img class="m-[5%] w-[90%]" src="https://www.fujitv.co.jp/ippon/img/logo_ogp.png" alt="IPPONグランプリ画像" >
+		<dev class=" flex">
+			<dev class="bg-lime-200 w-[40%] m-[5%] flex justify-center items-center sm:text-1xl md:text-2xl lg:text-3xl">
+				<button on:click={inviteLink}>招待リンク発行</button>
+			</dev>
+
+			<dev class="bg-lime-200 w-[40%] m-[5%] flex justify-center items-center sm:text-1xl md:text-2xl lg:text-3xl">
+				<button on:click={startGame}>ゲームスタート</button>
+			</dev>
+		</dev>
+    </div>
+</div>
+
+<style></style>
