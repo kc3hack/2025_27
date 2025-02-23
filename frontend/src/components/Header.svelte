88021f1ec0697360cd84ed4e4ed1177b2dc4831d
Neon-straySheep <skyskysky1228t@gmail.com>
2025-02-24 05:44:20 +0900

    透過写真完成しました！(今度こそ！)

--- frontend/src/components/Header.svelte
+++ frontend/src/components/Header.svelte
@@ -3,12 +3,5 @@
 </script>
 
 <header class="bg-amber-[400] header w-full flex justify-center">
-    <img src="/images/touka.png" alt="WARA1ロゴ" class="transparent-image" />
+    <img src="./images/nextTouka.png" alt="WARA1ロゴ" />
 </header>
-
-<style>
-    .transparent-image {
-      mix-blend-mode: multiply;
-      background-color: white; /* 画像の白を透過させるための背景色 */
-    }
-  </style>
