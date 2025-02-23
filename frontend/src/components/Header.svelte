8a812ff47bfb9ab801215f43b0de58d3498224d5
Neon-straySheep <skyskysky1228t@gmail.com>
2025-02-24 05:24:44 +0900

    透過ロゴ完成しました！！

--- frontend/src/components/Header.svelte
+++ frontend/src/components/Header.svelte
@@ -3,5 +3,12 @@
 </script>
 
 <header class="bg-amber-[400] header w-full flex justify-center">
-    <img src="/images/logoWARA1.png" alt="WARA1ロゴ">
+    <img src="/images/touka.png" alt="WARA1ロゴ" class="transparent-image" />
 </header>
+
+<style>
+    .transparent-image {
+      mix-blend-mode: multiply;
+      background-color: white; /* 画像の白を透過させるための背景色 */
+    }
+  </style>
