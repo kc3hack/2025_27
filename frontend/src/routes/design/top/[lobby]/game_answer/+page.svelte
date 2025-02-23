8e5911c90809fbb0004986057995233cad5e67d3
Hideyasu <kutuzov1228@gmail.com>
2025-02-24 00:23:20 +0900

    feat: build error fix

--- frontend/src/routes/design/top/[lobby]/game_answer/+page.svelte
+++ frontend/src/routes/design/top/[lobby]/game_answer/+page.svelte
@@ -1,48 +1,37 @@
-<script>  
-    import Canvas from './Canvas.svelte'
-	import Palette from './Palette.svelte'
+<script>
+	import Palette from './Palette.svelte';
 
-    const colors = [
-		'#d58141',
-		'#d7c44c',
-		'#4fa9cc',
-		'#3f8d27',
-	]
-	const background = '#fff'
+	const colors = ['#d58141', '#d7c44c', '#4fa9cc', '#3f8d27'];
+	const background = '#fff';
 
-	let color = colors[0]
-	const paletteColor = color
+	let color = colors[0];
+	const paletteColor = color;
 </script>
+
 <main class="content h-250 container mx-auto flex flex-col bg-amber-300">
-    <!-- お題表示の部分 -->
-    <section class= "w-full bg-yellow-500 p-15 text-5xl rounded-md text-center text-black">
-        お題:ボウリングで全ての投球がガターだった時、起こることは？
-    </section>
+	<!-- お題表示の部分 -->
+	<section class="p-15 w-full rounded-md bg-yellow-500 text-center text-5xl text-black">
+		お題:ボウリングで全ての投球がガターだった時、起こることは？
+	</section>
 
-    
-    <!-- お絵描き部分 -->
-        <div class="Drawing section w-1/2 bg-amber-600">
-            <Canvas {color} {background} />
-            <div class="palette w-1/2 bg-amber-500">
-            <Palette 
-                {paletteColor}
-                {colors}
-                {background}
-                on:color="{({ detail }) => {
-                    color = detail.color;
-                }}"
-            />
-            </div>
-        </div>
+	<!-- お絵描き部分 -->
+	<div class="Drawing section w-1/2 bg-amber-600">
+		<div class="palette w-1/2 bg-amber-500">
+			<Palette
+				{paletteColor}
+				{colors}
+				{background}
+				on:color={({ detail }) => {
+					color = detail.color;
+				}}
+			/>
+		</div>
+	</div>
 
-    <!-- 発表部分 -->
-    <div class= "bg-amber-200 p-5 flex justify-end fixed bottom-20 right-1">
-        <button class="px-10 py-3 bg-yellow-600 text-white rounded-md text-4xl hover:bg-orange-200">
-            発表する！
-        </button>
-    </div>
-    
-
+	<!-- 発表部分 -->
+	<div class="fixed bottom-20 right-1 flex justify-end bg-amber-200 p-5">
+		<button class="rounded-md bg-yellow-600 px-10 py-3 text-4xl text-white hover:bg-orange-200">
+			発表する！
+		</button>
+	</div>
 </main>
-
-
--- frontend/src/routes/design/top/[lobby]/practice/+page.svelte
+++ frontend/src/routes/design/top/[lobby]/practice/+page.svelte
@@ -1,34 +1,26 @@
-<script>  
-    import Canvas from '$src/components/Canvas.svelte'
-	import Palette from '$src/components/Palette.svelte'
+<script>
+	import Palette from '$src/components/Palette.svelte';
 
-    const colors = [
-		'#d58141',
-		'#d7c44c',
-		'#4fa9cc',
-		'#3f8d27',
-	]
-	const background = '#fff'
+	const colors = ['#d58141', '#d7c44c', '#4fa9cc', '#3f8d27'];
+	const background = '#fff';
 
-	let color = colors[0]
-	const paletteColor = color
+	let color = colors[0];
+	const paletteColor = color;
 </script>
 
 <section>
-    <div class="bg-blue-800 mx-10 mt-10 py-10 h-60">
-        <div class="bg-purple-400 h-full mx-10"></div>
-    </div>
+	<div class="mx-10 mt-10 h-60 bg-blue-800 py-10">
+		<div class="mx-10 h-full bg-purple-400"></div>
+	</div>
 </section>
 
-<section class="bg-yellow-500 mx-10 mt-10 py-10">
-    <div class="bg-red-500 h-120 mx-10 py-10 px-10 flex">
-        <div class="canvas w-7/10 bg-red-400 h-full mr-4">
-            <Canvas />
-        </div>
+<section class="mx-10 mt-10 bg-yellow-500 py-10">
+	<div class="h-120 mx-10 flex bg-red-500 px-10 py-10">
+		<div class="canvas w-7/10 mr-4 h-full bg-red-400"></div>
 
-        <div class="menu w-3/10 bg-red-100 h-full flex flex-col">
-            <Palette />
-            <div class="happyou h-10 w-full bg-purple-700 mt-auto flex" ></div>
-        </div>
-    </div>
+		<div class="menu w-3/10 flex h-full flex-col bg-red-100">
+			<Palette />
+			<div class="happyou mt-auto flex h-10 w-full bg-purple-700"></div>
+		</div>
+	</div>
 </section>
